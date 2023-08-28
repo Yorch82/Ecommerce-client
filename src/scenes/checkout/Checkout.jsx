b55c7c7a421df5413313ec31c8e9a0b3d5909796
Yorch82 <jcampo1982@outlook.com>
2023-08-28 12:55:23 +0200

    stripe

--- src/scenes/checkout/Checkout.jsx
+++ src/scenes/checkout/Checkout.jsx
@@ -6,6 +6,11 @@ import * as yup from "yup";
 import Shipping from "./Shipping"
 import { shades } from "../../theme";
 import Payment from "./Payment";
+import { loadStripe } from "@stripe/stripe-js"
+
+const stripePromise = loadStripe(
+    "pk_test_51Nk2k7Hdx0R6tWSnBYyhYVAve3Ft6EidCIdJOgR8kRoAA21IP2MkHi3Z1WewVrZ1uzrsRA4W0gOzuicJpgAIVWPN00YboPf1dw"
+)
 
 const initialValues = {
     billingAddress: {
@@ -109,7 +114,26 @@ const Checkout = () => {
     };
 
     async function makePayment(values) {
+        const stripe = await stripePromise;
+        const requestBody = {
+            userName: [values.firstName, values.lastName].join(" "),
+            email: values.email,
+            products: cart.map(({id, count}) => ({
+                id,
+                count,
+            }))
+        };
 
+        const response = await fetch("http://localhost:1337/api/orders", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(requestBody)
+        });
+
+        const session = await response.json();
+        await stripe.redirectToCheckout({
+            sessionId: session.id
+        })
     };
 
     return <Box width="80%" m="100px auto">
